8931628c9bc1d514d44b5aa5049eb7a7b5c2e1d8
taizilongxu <468137306@qq.com>
2014-08-06 16:22:11 +0800

    fix color

--- love.py
+++ love.py
@@ -1,21 +1,31 @@
-class Me(Xiao):
-    def __init__(self):
-        self.Lover = ''
-        self.Love = 0
-
-    def Myheart(self):
-        while True:
-            self.Love += 1
-
 def main():
-    # I become a developer.
-    Me = Me()
-    # when I met you,
-    # I didn't think you will stay in my life,
-    # but now I can't image how the world would be like without you!
-    Me.Lover = Lily
-    # I love you,forever.
-    Me.Myheart()
+    '''
+    Today is our 1st anniversary,but I have less skill of javascipt,
+    So I copy the site to write some code to you for our love story.
+    '''
+    I = boy(Xiao)
+    u = girl(Li)
+    # Summer of 2003,I know about you,
+    # you just a lovely girl and we can see each other through the window.
+    I.know(u)
+    # Sep 14, 2012,I meet you again in my college.
+    I.meet(u)
+    # The half of the year we conquer the hardships together.
+    I.together(u)
+    # I really feel bad when you leave me
+    I.mood = shit
+    # I  make sure that you are the right one
+    I.Lover = u
+    # Aug 13,2013,I date you,we have a dinner.
+    I.date(u)
+    # Sep 19,2013,the mid-autumn festival,I clearly remember the night and the moon.
+    # What a wonderful night.
+    I.kiss(u)
+    # I think you are the right one without doubt.
+    u.Lover = I
+    # I love you.
+    while True:
+        I.Love += 1
 
 if __name__ == '__main__':
     main()
