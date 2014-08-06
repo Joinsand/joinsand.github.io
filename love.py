589413f62e1b336e7c59800244276f91f888a433
taizilongxu <468137306@qq.com>
2014-08-06 08:04:04 +0800

    add

---
+++ love.py
@@ -0,0 +1,21 @@
+class Me(Xiao):
+    def __init__(self):
+        self.Lover = ''
+        self.Love = 0
+
+    def Myheart(self):
+        while True:
+            self.Love += 1
+
+def main():
+    # I become a developer.
+    Me = Me()
+    # when I met you,
+    # I didn't think you will stay in my life,
+    # but now I can't image how the world would be like without you!
+    Me.Lover = Lily
+    # I love you,forever.
+    Me.Myheart()
+
+if __name__ == '__main__':
+    main()
